c1f16c794b86315a39293e9bbf565a67aa8acc78
Qiu <qiujunsen@163.com>
2018-12-08 14:49:01 +0800

    完善模拟号码

--- Invoker/liang.py
+++ Invoker/liang.py
@@ -21,16 +21,11 @@ def refresh_data():
 	fetch.refresh_answer()
 
 
-def mock(no, count):
-	if len(no) < 7:
+def mock(no, end_no):
+	if len(no) < 7 or len(end_no) < 7:
 		print('期号输入错误, 正确格式181010001')
 		return None
 	print('开始模拟数据...')
-
-	day_no = no[6:]
-	second = int(time.mktime(time.strptime(no[0:6], '%y%m%d')))
-	second += int(int(day_no) + int(count) / 120) * 86400
-	end_no = time.strftime('%y%m%d', time.localtime(second)) + str(int(day_no) + int(count) % 120)
 
 	award_db = AwardObject()
 	return award_db.get_new_award(no, end_no)
@@ -42,14 +37,20 @@ def output_csv(mock_data, file_name):
 		return
 	print('开始生成数据...')
 	familiar_map = {'myriad': [], 'thousand': [], 'hundred': [], 'ten': [], 'one': []}
+	count_map = {'myriad': {}, 'thousand': {}, 'hundred': {}, 'ten': {}, 'one': {}}
 	with open(file_name, 'w+') as csv_file:
 		writer = csv.writer(csv_file)
-		writer.writerow(['期号', '万', '千', '百', '十', '个', '万熟组', '千熟组', '百熟组', '十熟组', '个熟组', '万生', '千生', '百生', '十生', '个生'])
+		writer.writerow([
+			'期号', '万', '千', '百', '十', '个',
+			'万熟组', '千熟组', '百熟组', '十熟组', '个熟组',
+			'万生', '千生', '百生', '十生', '个生',
+			'万码组', '千码组', '百码组', '十码组', '个码组'
+		])
 		for item in mock_data:
-			writer.writerow(update_familiar(familiar_map, item))
+			writer.writerow(update_familiar(familiar_map, count_map, item))
 
 
-def update_familiar(familiar_map, item):
+def update_familiar(familiar_map, count_map, item):
 	award = item['number'].replace(' ', '')
 	if award and len(award) == 5:
 		myriad, thousand, hundred, ten, one = (award[0], award[1], award[2], award[3], award[4])
@@ -61,24 +62,32 @@ def update_familiar(familiar_map, item):
 		for (key, value) in familiar_map.items():
 			if key == 'myriad':
 				sort_familiar(myriad, value)
+				count(myriad, count_map[key])
 			elif key == 'thousand':
 				sort_familiar(thousand, value)
+				count(thousand, count_map[key])
 			elif key == 'hundred':
 				sort_familiar(hundred, value)
+				count(hundred, count_map[key])
 			elif key == 'ten':
 				sort_familiar(ten, value)
+				count(ten, count_map[key])
 			elif key == 'one':
 				sort_familiar(one, value)
+				count(one, count_map[key])
 		return [
 			item['no'], myriad, thousand, hundred, ten, one, familiar_map.get('myriad'), familiar_map.get('thousand'),
 			familiar_map.get('hundred'), familiar_map.get('ten'), familiar_map.get('one'),
-			if_myriad, is_thousand, is_hundred, is_ten, is_one
+			if_myriad, is_thousand, is_hundred, is_ten, is_one, count_map.get('myriad'), count_map.get('thousand'),
+			count_map.get('hundred'), count_map.get('ten'), count_map.get('one')
 		]
 	else:
 		return [
 			item['no'], '', '', '', '', '', familiar_map.get('myriad'), familiar_map.get('thousand'),
 			familiar_map.get('hundred'), familiar_map.get('ten'), familiar_map.get('one'), familiar_map.get('myriad'),
-			familiar_map.get('thousand'), familiar_map.get('hundred'), familiar_map.get('ten'), familiar_map.get('one')
+			familiar_map.get('thousand'), familiar_map.get('hundred'), familiar_map.get('ten'), familiar_map.get('one'),
+			count_map.get('myriad'), count_map.get('thousand'), count_map.get('hundred'),
+			count_map.get('ten'), count_map.get('one')
 		]
 
 
@@ -89,6 +98,11 @@ def sort_familiar(number, value):
 			value.append(number)
 	else:
 		value.append(number)
+
+
+def count(number, value):
+	count_number = value.get(number, 0)
+	value[number] = count_number + 1
 
 
 def is_unfamiliar(number, value):
@@ -103,11 +117,11 @@ if __name__ == '__main__':
 	complete = False
 	while not complete:
 		py_no = input('请输入开始模拟期号: 如181010120')
-		py_count = input('请输入模拟期数: 如500')
-		if py_no and py_count:
-			data = mock(py_no, py_count)
+		py_end_no = input('请输入结束模拟期号: 如181030120')
+		if py_no and py_end_no:
+			data = mock(py_no, py_end_no)
 			if data:
-				output_csv(data, '%s-%s.csv' % (py_no, py_count))
+				output_csv(data, '%s-%s.csv' % (py_no, py_end_no))
 				complete = True
 		else:
 			print('输入格式有误')
